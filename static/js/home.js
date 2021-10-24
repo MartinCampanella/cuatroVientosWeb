05e26368fa48394d02e56d277dd175adc5fc198b
MartinCampanella <martin.campanella@despegar.com>
2021-10-24 15:57:31 -0300

    Mas fotos en galerias

--- static/js/home.js
+++ static/js/home.js
@@ -45,7 +45,7 @@ function loadGaleriaCabana2() {
 }
 
 function loadGaleriaPredio(){
-    var imagenes = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+    var imagenes = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
     var galeria = document.getElementById('galeria-predio');
 
     for(imagen of imagenes){
