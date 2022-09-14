24bd1a99eab0d95fbba9432a15504e831b7c410d
MartinCampanella <martin.campanella.93@gmail.com>
2022-09-14 20:30:50 -0300

    cabana 4 y pileta

--- static/js/home.js
+++ static/js/home.js
@@ -22,7 +22,7 @@ function loadGaleriaCabana1() {
 }
 
 function loadGaleriaCabana2() {
-    var imagenes = [1,2,3,4,5,6,7,8,9,10,11,12];
+    var imagenes = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
     var galeria = document.getElementById('galeria-cabana-2');
 
     for(imagen of imagenes){
@@ -45,7 +45,7 @@ function loadGaleriaCabana2() {
 }
 
 function loadGaleriaPredio(){
-    var imagenes = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
+    var imagenes = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32];
     var galeria = document.getElementById('galeria-predio');
 
     for(imagen of imagenes){
